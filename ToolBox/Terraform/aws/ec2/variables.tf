9f6ecfe3ab4a3af0774ed817d12a34c38c719f15
sapelsin <volodymyrhorshkov@gmail.com>
2021-04-24 21:37:31 +0300

    first commit

---
+++ ToolBox/Terraform/aws/ec2/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_access_key" {
+  type    = string
+  default = "AKIAZVOX67YATLIX6VGW"
+}
+variable "aws_secret_key" {
+  type    = string
+  default = "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"
+}
